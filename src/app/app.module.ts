e59c0dfdbdce1c23ed74d65a012531a5e8ad7b15
Omie Walls <omie.walls@daugherty.com>
2018-08-31 19:33:38 -0400

    Basic components added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { MainComponent } from './main/main.component';
+import { ArticleComponent } from './article/article.component';
+import { FooterComponent } from './footer/footer.component';
+import { NavComponent } from './header/nav/nav.component';
+import { PageNotFoundComponent } from './page.not.found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MainComponent,
+    ArticleComponent,
+    FooterComponent,
+    NavComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule
